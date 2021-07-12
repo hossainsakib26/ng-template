6592c1f74802855d5cf9c34e9cd4cdd9a746b665
lifeoflikhon <mehedi.hasansjs@gmail.com>
2021-07-12 12:57:45 +0600

    added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   important: true,
   purge: {
@@ -6,7 +8,20 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gotham: "'Gotham', sans-serif",
+        gotham100: "'Gotham Thin', sans-serif",
+        gotham200: "'Gotham XLight', sans-serif",
+        gotham300: "'Gotham Light', sans-serif",
+        gotham400: "'Gotham', sans-serif",
+        gotham500: "'Gotham Medium', sans-serif",
+        gotham600: "'Gotham Bold', sans-serif",
+        gotham700: "'Gotham Black', sans-serif",
+        gotham800: "'Gotham Ultra', sans-serif",
+        dankMono: "'Dank Mono', monospace",
+      }
+    },
   },
   variants: {
     extend: {},
